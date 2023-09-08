ea39121703541748d290e18433fd90e1d781416b
ntsee <29243779+ntsee@users.noreply.github.com>
2023-09-08 10:39:30 -0400

    Replaced sort.s program example with textbook example

--- examples/sort_optimized.s
+++ examples/sort_optimized.s
@@ -2,8 +2,7 @@
 	numbers: .8byte 9, 8, 2, 3, 6
 
 .text
-    .global _start
-
+.global _start
 _start:
 main:
 	ldur x0, =numbers
@@ -16,8 +15,8 @@ swap:
 	lsl x10, x1, #3
 	add x10, x0, x10
 	ldur x9, [x10, #0]
-	ldur x11, [x10, #8]
-	stur x11, [x10, #0]
+	ldur x11,[x10, #8]
+	stur x11,[x10, #0]
 	stur x9, [x10, #8]
 	ret
 
@@ -27,38 +26,29 @@ sort:
 	stur x21, [sp, #16]
 	stur x20, [sp, #8]
 	stur x19, [sp, #0]
-	mov x19, xzr
+	mov x14, xzr
 
 for1tst:
-	cmp x19, x1
+	cmp x14, x1
 	b.ge exit1
-	mov x22, x1
-	sub x20, x19, #1
+	sub x15, x14, #1
 
 for2tst:
-	cmp x20, xzr
+	cmp x15, xzr
 	b.lt exit2
-	lsl x10, x20, #3
+	lsl x10, x15, #3
 	add x11, x0, x10
 	ldur x12, [x11, #0]
 	ldur x13, [x11, #8]
 	cmp x12, x13
 	b.le exit2
-	mov x1, x20
-	bl swap
-	sub x20, x20, #1
+	stur x12, [x11, #8]
+	stur x13, [x11, #0]
+	sub x15, x15, #1
 	b for2tst
 
 exit2:
-	add x19, x19, #1
+	add x14, x14, #1
 	b for1tst
-
 exit1:
-	ldur x19, [sp, #0]
-	ldur x20, [sp, #8]
-	ldur x21, [sp, #16]
-	ldur lr, [sp, #24]
-	add sp, sp, #32
 	ret
-
-
